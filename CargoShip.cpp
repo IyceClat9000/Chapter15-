8474ab2ff3e5ffb56d6ffb24fcb878187869b079
Tuniphn <phantuanthanh2006@gmail.com>
2026-04-15 21:26:48 -0700

    CargoShip.cpp
    Full final version

--- CargoShip.cpp
+++ CargoShip.cpp
@@ -1,26 +1,36 @@
 #include "CargoShip.h"
 
 //constructor
+//Precondition: N/A
+//Postcondition: Set the private capacity of cargo ship to 0
 CargoShip::CargoShip()
 {
 	cargoCapacity = 0;
 }
 //arguement constructor
+//Precondition: a valid new string shipName, builtYear, and Cargo Capacity
+//Postcondition: Chaining constructor Set the private shipName, yearBuilt, cargo capacity to new shipName, yearBuilt, cargo capacity passed in
 CargoShip::CargoShip(string newShipName, string newYearBuilt, int newCargoCapacity) : Ship(newShipName, newYearBuilt)
 {
 	cargoCapacity = newCargoCapacity;
 }
 
 //setters and getters
+//Precondition: a valid new integer capacity
+//Postcondition: Set the private capacity to new capacity passed in
 void CargoShip::setCargoCapacity(int newCargoCapacity)
 {
 	cargoCapacity = newCargoCapacity;
 }
+//Precondition: N/A
+//Postcondition: Return the private cargoCapacity
 int CargoShip::getCargoCapacity() const
 {
 	return cargoCapacity;
 }
 
+//Precondition: N/A
+//Postcondition: Overrided print function that works for cargoShip object
 void CargoShip::print() const
 {
 	cout << "\n\n\tCargoShip information: ";
