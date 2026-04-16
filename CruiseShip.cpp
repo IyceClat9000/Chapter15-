f7c74893a9ec78b9103195954045f413243e98c4
Tuniphn <phantuanthanh2006@gmail.com>
2026-04-15 21:27:15 -0700

    CruiseShip.cpp
    Full final version

--- CruiseShip.cpp
+++ CruiseShip.cpp
@@ -1,26 +1,36 @@
 #include "CruiseShip.h"
 
 //constructor
+//Precondition: N/A
+//Postcondition: Set the private maximumPassenger of Cruise ship to 0
 CruiseShip::CruiseShip()
 {
 	maximumPassengers = 0;
 }
 //arguement constructor
+//Precondition: a valid new string shipName, builtYear, and maximumPassenger
+//Postcondition: Chaining constructor Set the private shipName, yearBuilt, maximumPassenger to new shipName, yearBuilt, maximumPassenger passed in
 CruiseShip::CruiseShip(string newShipName, string newYearBuilt, int newMaximumPassenger) : Ship(newShipName, newYearBuilt)
 {
 	maximumPassengers = newMaximumPassenger;
 }
 
 //setters and getters
+//Precondition: a valid new integer maximumPassenger
+//Postcondition: Set the private maximumPassenger to new maximumPassenger passed in
 void CruiseShip::setMaximumPassengers(int newMaximumPassenger)
 {
 	maximumPassengers = newMaximumPassenger;
 }
+//Precondition: N/A
+//Postcondition: Return the private maximumPassenger
 int CruiseShip::getMaximumPassengers() const
 {
 	return maximumPassengers;
 }
 
+//Precondition: N/A
+//Postcondition: Overrided print function that works for cruiseship object
 void CruiseShip::print() const
 {
 	cout << "\n\n\tCruiseShip information: ";
